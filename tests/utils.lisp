fec99a77a04b0ec679187e89c7f0cb11d4688172
Eitaro Fukamachi <e.arrows@gmail.com>
2019-07-28 19:26:39 +0900

    Fix utils tests.

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -9,16 +9,11 @@
 (defstruct person name)
 
 (deftest proper-list-tests
-  (setf *proper-list-type-checker*
-        (make-hash-table :test 'equal))
-
   (ok (typep nil 'proper-list))
   (ok (typep '(1) 'proper-list))
   (ok (typep '(1 2) 'proper-list))
   (ok (typep '(1 (2)) 'proper-list))
   (ng (typep '(1 . 2) 'proper-list))
-
-  (ok (= (hash-table-count *proper-list-type-checker*) 0))
 
   (ok (typep nil '(proper-list integer)))
   (ok (typep '(1) '(proper-list integer)))
@@ -27,11 +22,7 @@
   (ng (typep '(1 . 2) '(proper-list integer)))
   (ng (typep '(#\a #\b) '(proper-list integer)))
 
-  (ok (= (hash-table-count *proper-list-type-checker*) 1))
-
   (ok (typep nil '(proper-list person)))
   (ok (typep (list (make-person :name "Eitaro"))
              '(proper-list person)))
-  (ng (typep '(1) '(proper-list person)))
-
-  (ok (= (hash-table-count *proper-list-type-checker*) 2)))
+  (ng (typep '(1) '(proper-list person))))
