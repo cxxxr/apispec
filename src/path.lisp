ef6e757ea0988929f9dc0ecb6533e784fed78ec5
Eitaro Fukamachi <e.arrows@gmail.com>
2019-08-08 02:53:25 +0900

    Reorganize operation/path package and files.

--- src/path.lisp
+++ src/path.lisp
@@ -1,38 +1,74 @@
-(uiop:define-package #:apispec/path
+(defpackage #:apispec/path
   (:use #:cl
         #:apispec/utils)
-  (:use-reexport #:apispec/path/item)
-  (:import-from #:assoc-utils
-                #:aget))
+  (:import-from #:apispec/request
+                #:parameter)
+  (:import-from #:apispec/operation
+                #:operation)
+  (:export #:path-item
+           #:path-item-summary
+           #:path-item-description
+           #:path-item-parameters
+           #:path-item-get
+           #:path-item-put
+           #:path-item-post
+           #:path-item-delete
+           #:path-item-options
+           #:path-item-head
+           #:path-item-patch
+           #:path-item-trace
+           #:paths))
 (in-package #:apispec/path)
 
 (declaim-safety)
 
-(defclass mapper ()
-  ((%paths :type paths
-           :initform '())))
+(defclass path-item ()
+  ((summary :type (or string null)
+            :initarg :summary
+            :initform nil
+            :reader path-item-summary)
+   (description :type (or string null)
+                :initarg :description
+                :initform nil
+                :reader path-item-description)
+   (parameters :type (proper-list parameter)
+               :initarg :parameters
+               :initform nil
+               :reader path-item-parameters)
+   (get :type (or operation null)
+        :initarg :get
+        :initform nil
+        :accessor path-item-get)
+   (put :type (or operation null)
+        :initarg :put
+        :initform nil
+        :accessor path-item-put)
+   (post :type (or operation null)
+         :initarg :post
+         :initform nil
+         :accessor path-item-post)
+   (delete :type (or operation null)
+           :initarg :delete
+           :initform nil
+           :accessor path-item-delete)
+   (options :type (or operation null)
+            :initarg :options
+            :initform nil
+            :accessor path-item-options)
+   (head :type (or operation null)
+         :initarg :head
+         :initform nil
+         :accessor path-item-head)
+   (patch :type (or operation null)
+          :initarg :patch
+          :initform nil
+          :accessor path-item-patch)
+   (trace :type (or operation null)
+          :initarg :trace
+          :initform nil
+          :accessor path-item-trace)))
 
-(defun add-operation (mapper method path-rule operation)
-  (check-type path-rule string)
-  (let* ((paths (slot-value mapper '%paths))
-         (path-item
-           (or (aget paths path-rule)
-               (let ((new-item (make-instance 'path-item)))
-                 (setf (aget paths path-rule)
-                       new-item)
-                 (setf (slot-value mapper '%paths) paths)
-                 new-item))))
-    (funcall
-      (ecase method
-        (:get #'(setf path-item-get))
-        (:put #'(setf path-item-put))
-        (:post #'(setf path-item-post))
-        (:delete #'(setf path-item-delete))
-        (:options #'(setf path-item-options))
-        (:head #'(setf path-item-head))
-        (:patch #'(setf path-item-patch))
-        (:trace #'(setf path-item-trace)))
-      operation
-      path-item)))
+(deftype paths ()
+  '(association-list string path-item))
 
 (undeclaim-safety)
