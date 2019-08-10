7700559dd79240a3d0df4331805015d76456e7cf
Eitaro Fukamachi <e.arrows@gmail.com>
2019-08-11 05:20:55 +0900

    Simplify types/header.

--- src/types/header.lisp
+++ src/types/header.lisp
@@ -6,19 +6,10 @@
   (:export #:header
            #:header-required-p
            #:header-schema
-           #:header-style
            #:header-explode-p))
 (in-package #:apispec/types/header)
 
 (declaim-safety)
-
-(defun header-style-string-p (style)
-  (and (member style '("matrix" "label" "form" "simple" "spaceDelimited" "pipeDelimited" "deepObject")
-               :test #'equal)
-       t))
-
-(deftype header-style ()
-  '(satisfies header-style-string-p))
 
 (defclass header ()
   ((required :type boolean
@@ -29,20 +20,9 @@
            :initarg :schema
            :initform nil
            :reader header-schema)
-   (style :type header-style
-          :initarg :style
-          :initform "simple"
-          :reader header-style)
    (explode :type boolean
-            :initarg :explode)))
-
-(defun header-explode-p (header)
-  (check-type header header)
-  (if (slot-boundp header 'explode)
-      (slot-value header 'explode)
-      (let ((style (header-style header)))
-        (if (equal style "form")
-            t
-            nil))))
+            :initarg :explode
+            :initform nil
+            :reader header-explode-p)))
 
 (undeclaim-safety)
